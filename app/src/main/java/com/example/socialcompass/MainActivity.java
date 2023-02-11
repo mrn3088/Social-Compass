7a43991d71dea1bf9e89983f568ded3affea3730
abigailshilts <abbi@shilts.net>
2023-02-10 17:18:26 -0800

    Retrieves current location, and will update as location changes

--- app/src/main/java/com/example/socialcompass/MainActivity.java
+++ app/src/main/java/com/example/socialcompass/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.socialcompass;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     // IF SHARED PREFERENCES DON'T EXIST STAY ON PAGE
     // ELSE IMMEDAITLY LEAVE MAIN ACTIVITY AND LOAD MAP!
+
+    private LocationService locationService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,16 @@ public class MainActivity extends AppCompatActivity {
         if(false) {
             startActivity(intent);
         }
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 200);
+        }
+        locationService = LocationService.singleton(this);
+        TextView textView = (TextView) findViewById(R.id.coordinatesDisplay);
+        locationService.getLocation().observe(this, loc ->{
+            textView.setText(Double.toString(loc.first) + " , " + Double.toString(loc.second));
+        });
     }
 
     public void onSubmitLabelsClicked(View view) {
--- app/src/main/java/com/example/socialcompass/ShowMapActivity.java
+++ app/src/main/java/com/example/socialcompass/ShowMapActivity.java
@@ -7,24 +7,15 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
+
 public class ShowMapActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_map);
 
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-            && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 200);
-        }
-
-        locationService.LocationService.singleton(this);
-        TextView textView = (TextView) findViewById(R.id.coordinatesDisplay);
-
-        locationService.getLocation().observe(this, loc ->{
-            textView.setText(Double.toString(loc.first) + " , " + Double.toString(loc.second));
-        });
     }
 }
