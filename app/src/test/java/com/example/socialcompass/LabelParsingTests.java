2b913c2609d74f6d47dad59b6e188f9ad1b635e6
Hexachlorocyclohexane3088 <97484148+Hexachlorocyclohexane3088@users.noreply.github.com>
2023-02-13 01:53:40 -0800

    added ReletiveAngleTest

--- app/src/test/java/com/example/socialcompass/LabelParsingTests.java
+++ app/src/test/java/com/example/socialcompass/LabelParsingTests.java
@@ -130,4 +130,92 @@ public class LabelParsingTests {
     }
 
 
+    public static class RelativeAngleTests {
+        @Test
+        public void testRadiansToDegreeFloat(){
+            double offset = 0.1;
+            double rad1 = 0.0;
+            double expectedAngle1 = 0.0;
+            double realAngle1 = Utilities.radiansToDegreesDouble(rad1);
+
+            if(realAngle1>=expectedAngle1-offset && realAngle1<=expectedAngle1+offset){
+                assertTrue(true);
+            }else{
+                assertTrue(false);
+            }
+
+            double rad2 = -2.324;
+            double expectedAngle2 = -133.155;
+            double realAngle2 = Utilities.radiansToDegreesDouble(rad2);
+            if(realAngle2>=expectedAngle2-offset && realAngle2<=expectedAngle2+offset){
+                assertTrue(true);
+            }else{
+                assertTrue(false);
+            }
+
+            double rad3 = 3.14;
+            double expectedAngle3 = 179.909;
+            double realAngle3 = Utilities.radiansToDegreesDouble(rad3);
+            if(realAngle3>=expectedAngle3-offset && realAngle3<=expectedAngle3+offset){
+                assertTrue(true);
+            }else{
+                assertTrue(false);
+            }
+
+            double rad4 = 10.6;
+            double expectedAngle4 = 607.335;
+            double realAngle4 = Utilities.radiansToDegreesDouble(rad4);
+            if(realAngle4>=expectedAngle4-offset && realAngle4<=expectedAngle4+offset){
+                assertTrue(true);
+            }else{
+                assertTrue(false);
+            }
+        }
+
+        @Test
+        public void testRadiansToDegreeDouble(){
+            float offset = 0.1f;
+            float rad1 = 0.0f;
+            float expectedAngle1 = 0.0f;
+            float realAngle1 = Utilities.radiansToDegreesFloat(rad1);
+
+            if(realAngle1>=expectedAngle1-offset && realAngle1<=expectedAngle1+offset){
+                assertTrue(true);
+            }else{
+                assertTrue(false);
+            }
+
+            float rad2 = -2.324f;
+            float expectedAngle2 = -133.155f;
+            float realAngle2 = Utilities.radiansToDegreesFloat(rad2);
+            if(realAngle2>=expectedAngle2-offset && realAngle2<=expectedAngle2+offset){
+                assertTrue(true);
+            }else{
+                assertTrue(false);
+            }
+
+            float rad3 = 3.14f;
+            float expectedAngle3 = 179.909f;
+            float realAngle3 = Utilities.radiansToDegreesFloat(rad3);
+            if(realAngle3>=expectedAngle3-offset && realAngle3<=expectedAngle3+offset){
+                assertTrue(true);
+            }else{
+                assertTrue(false);
+            }
+
+            float rad4 = 10.6f;
+            float expectedAngle4 = 607.335f;
+            float realAngle4 = Utilities.radiansToDegreesFloat(rad4);
+            if(realAngle4>=expectedAngle4-offset && realAngle4<=expectedAngle4+offset){
+                assertTrue(true);
+            }else{
+                assertTrue(false);
+            }
+        }
+
+        @Test
+        public void testRelativeAngle(){
+
+        }
+    }
 }
