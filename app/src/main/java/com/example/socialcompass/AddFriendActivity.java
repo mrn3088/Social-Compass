8b2f87ed9da8b6123f8afd46fe0209f45b5ccc55
xiuyuan0216 <x1yuan@ucsd.edu>
2023-03-04 23:32:41 -0800

    add friend UI

---
+++ app/src/main/java/com/example/socialcompass/AddFriendActivity.java
@@ -0,0 +1,17 @@
+package com.example.socialcompass;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddFriendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+}
