e1091fa8f13776dc48a49e4aee17bb9263a43456
yuelei0428 <93293009+yuelei0428@users.noreply.github.com>
2023-03-08 22:20:08 -0800

    Finished all ms2 iteration1 test

---
+++ app/src/test/java/com/example/socialcompass/MS2Story2Test.java
@@ -0,0 +1,26 @@
+package com.example.socialcompass;
+
+import static com.example.socialcompass.Utilities.calculateDistance;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MS2Story2Test {
+
+    @Test
+    public void calculateDistanceTest(){
+
+        double lat1 = 0.0;
+        double long1 = 0.0;
+        double lat2 = 90.0;
+        double long2 = 90.0;
+        double calculatedDistance = 0;
+        double expectedDistance = 10007.54;
+
+        calculatedDistance = Utilities.calculateDistance(lat1, long1,lat2,long2);
+
+        assertEquals(expectedDistance, calculatedDistance, 0.02);
+    }
+
+
+}
