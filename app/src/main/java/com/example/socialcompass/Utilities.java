ee0689f4027cac9a2ebbf30c6eede887bd7103c4
yuelei0428 <93293009+yuelei0428@users.noreply.github.com>
2023-03-08 22:28:00 -0800

    updated utilities file

--- app/src/main/java/com/example/socialcompass/Utilities.java
+++ app/src/main/java/com/example/socialcompass/Utilities.java
@@ -217,6 +217,10 @@ public class Utilities {
     public static String getDisplayStr(String str) {
         return valueDisplayMap.getOrDefault(str, str);
     }
+
+    public static double calculateDistance(double lat1, double long1, double lat2, double long2){
+        return 6371.0*Math.acos(Math.cos((long1/180.0)*Math.PI)*Math.cos((long2/180.0)*Math.PI)*Math.cos(((lat1-lat2)/180.0))*Math.PI+Math.sin((lat1/180.0)*Math.PI)*Math.sin((lat2/180.0)*Math.PI));
+    }
 }
 
 
